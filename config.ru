d0831ea82b9d0cc8a24bcff09907d6fafdf8bd7e
Eugene <Re4port@ukr.net>
2017-06-16 16:39:10 +0300

    move header/footer to separete file, add couple of views, add cookies

--- config.ru
+++ config.ru
@@ -1,4 +1,11 @@
 require './lib/game'
-use Rack::Static, :urls => ["/images", "/styles"], :root => "public"
 
-run Game.new
+app = Rack::Builder.new do
+  use Rack::Static, :urls => ["/images", "/styles"], :root => "public"
+  run Game
+end
+
+run app
+#use Rack::Auth::Basic, "Restricted Area" do |username, password|
+#  [username, password] == ['code', 'breaker']
+#end
--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,43 @@
 require 'erb'
+require 'json'
 
 class Game
-  def call(env)
-    Rack::Response.new(render('index.html.erb'))
+  def self.call(env)
+    new(env).response.finish
+  end
+
+  def initialize(env)
+    @request = Rack::Request.new(env)
+    @layout = 'layout.html.erb'
+    @attempts = []
+  end
+
+  def response
+    case @request.path
+    when '/' then Rack::Response.new(render('index.html.erb'))
+    when '/new_game' then Rack::Response.new(render('index.html.erb'))
+    when '/try' then
+       Rack::Response.new do |response|
+        @try = @request.params['attempt'] # need send it to 'game.compare_with()'
+        #response.delete_cookie('name_of_cookie')
+        response.set_cookie('story', [['1234', '+-'], ['4453', '++-'], ['4453', '++-']].to_json)
+        p @attempts = JSON.parse(@request.cookies['story'])
+        response.write(render('index.html.erb'))
+      end
+    when '/about' then Rack::Response.new(render('about.html.erb'))
+    else Rack::Response.new(render('not_found.html.erb'))
+    end
   end
 
   def render(template)
-    path = File.expand_path("../../views/#{template}", __FILE__)
-    ERB.new(File.read(path)).result(binding)
+    templates = [template, @layout]
+    templates.inject(nil) do | prev, temp |
+      _render(temp) { prev }
+    end
+  end
+
+  def _render temp
+    path = File.expand_path("../../views/#{temp}", __FILE__)
+    ERB.new(File.read(path)).result( binding )
   end
 end
